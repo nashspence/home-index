f94b506a9bc175d23f3714ab320ae54dd28b6276
Nash Spence <nashspence@gmail.com>
2025-06-24 18:33:12 -0700

    Extract migrations to F2 module

--- features/F2/__init__.py
+++ features/F2/__init__.py
@@ -1,5 +1,5 @@
 """Feature F2 package."""
 
-from . import duplicate_finder, metadata_store, path_links
+from . import duplicate_finder, metadata_store, path_links, migrations
 
-__all__ = ["metadata_store", "path_links", "duplicate_finder"]
+__all__ = ["metadata_store", "path_links", "duplicate_finder", "migrations"]
