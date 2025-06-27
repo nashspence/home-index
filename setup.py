0b9ea281ea35b366790ac47bf19843e072b188b2
Nash Spence <nashspence@gmail.com>
2025-06-26 17:40:42 -0700

    Fix packaging for F4 modules

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup, find_packages
+from setuptools import setup, find_namespace_packages
 
 setup(
     name="home_index",
@@ -7,11 +7,11 @@ setup(
     author="Nash Spence",
     author_email="nashspence@gmail.com",
     url="https://github.com/nashspence/home-index",
-    packages=find_packages(where="packages")
-    + find_packages(where="features/F4", include=["home_index_module*"]),
+    packages=find_namespace_packages(where="packages")
+    + find_namespace_packages(where=".", include=["features*"], exclude=["*.test*"]),
     package_dir={
-        "": "packages",
-        "home_index_module": "features/F4/home_index_module",
+        "": ".",
+        "home_index": "packages/home_index",
     },
     python_requires=">=3.8",
 )
