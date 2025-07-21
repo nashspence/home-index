f14b487fd2cf1465b1cd7655bddc8a346a2e909a
Nash Spence <nashspence@gmail.com>
2025-07-20 20:40:06 -0700

    fix tests by restoring real apscheduler for acceptance

--- features/conftest.py
+++ features/conftest.py
@@ -10,6 +10,27 @@ def stub_dependencies(monkeypatch):
     import importlib
     import sys
 
+    modules_to_restore = [
+        "apscheduler",
+        "apscheduler.schedulers",
+        "apscheduler.schedulers.background",
+        "apscheduler.triggers",
+        "apscheduler.triggers.interval",
+        "apscheduler.triggers.cron",
+    ]
+    originals = {mod: sys.modules.get(mod) for mod in modules_to_restore}
+    try:
+        for mod in modules_to_restore:
+            sys.modules.pop(mod, None)
+        import apscheduler  # noqa: F401
+    except ModuleNotFoundError:
+        for mod, value in originals.items():
+            if value is not None:
+                sys.modules[mod] = value
+    else:
+        for mod in modules_to_restore:
+            sys.modules[mod] = importlib.import_module(mod)
+
     sys.modules.pop("features.F1", None)
     import features.F1 as f1
 
