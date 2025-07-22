6b7c0be94a2046ce65c29affaa5557780d105407
Nash Spence <nashspence@gmail.com>
2025-07-21 17:09:35 -0700

    chore: move tests under unified structure (#147)

---
+++ features/F1/tests/acceptance/test_acceptance.py
@@ -0,0 +1,19 @@
+from .test_s1 import f1s1
+from .test_s2 import f1s2
+from .test_s3 import f1s3
+from .test_s4 import f1s4
+from .test_s5 import f1s5
+from .test_s6 import f1s6
+from .test_s7 import f1s7
+from .test_s8 import f1s8
+
+__all__ = [
+    "f1s1",
+    "f1s2",
+    "f1s3",
+    "f1s4",
+    "f1s5",
+    "f1s6",
+    "f1s7",
+    "f1s8",
+]
---
+++ features/F3/tests/acceptance/test_acceptance.py
@@ -0,0 +1,25 @@
+from .test_s1 import f3s1
+from .test_s2 import f3s2
+from .test_s3 import f3s3
+from .test_s4 import f3s4
+from .test_s5 import f3s5
+from .test_s6 import f3s6
+from .test_s7 import f3s7
+from .test_s8 import f3s8
+from .test_s9 import f3s9
+from .test_s10 import f3s10
+from .test_s11 import f3s11
+
+__all__ = [
+    "f3s1",
+    "f3s2",
+    "f3s3",
+    "f3s4",
+    "f3s5",
+    "f3s6",
+    "f3s7",
+    "f3s8",
+    "f3s9",
+    "f3s10",
+    "f3s11",
+]
